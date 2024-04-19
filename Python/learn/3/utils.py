af1f088f36f7aed0a01b0a6431876bf39121cbe0
MoguchiyDD <ket-ket-01@bk.ru>
2024-04-19 19:06:54 +0500

    Adding UTILS #3

---
+++ Python/learn/3/utils.py
@@ -0,0 +1,33 @@
+# Developer && Owner: МогучийДД (MoguchiyDD)
+# LICENSE: MIT License which is located in the text file LICENSE
+#
+# Goal: Keep CONSTANTS for The SOFTWARE
+# Result: Saved CONSTANTS for The SOFTWARE
+#
+# Past Modification: Adding COPYRIGHT
+# Last Modification: Adding The «HEIGHT» Block
+# Modification Date: 2024.04.19, 01:05 PM
+#
+# Create Date: 2024.04.19, 01:04 PM
+
+
+from settings import HEIGHT
+
+
+# ------------ HEIGHT ------------
+
+def height_div(div: int) -> int:
+    """
+    Calculates The HEIGHT of a Block using a Divider
+
+    ---
+    PARAMETERS:
+    - div: int -> Divider for HEIGHT Calculation
+    ---
+    RESULTS: HEIGHT ( e.g. 576) // divs (e.g. 12) => 48
+    """
+
+    result = HEIGHT // div
+    return result
+
+# --------------------------------
